541d8ae6b0b0c2bb9e87590d2636ef186f953475
anupamroy777 <github.769@gmail.com>
2022-02-06 12:35:27 +0000

    anykernel: changed the string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ThunderStorm Kernel
+kernel.string=Welcome To The ThunderStorm
 do.devicecheck=1
 do.modules=0
 do.systemless=1
