0a74fe6534fd3032e3eaff265364afd37ef7e26d
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2022-11-27 23:28:25 +0530

    Update Build Info

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Welcome To The ThunderStorm
+kernel.string=Welcome To The Azrael ...
 do.devicecheck=1
 do.modules=0
 do.systemless=1
