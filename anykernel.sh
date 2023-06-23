5d8999c9070ea329da59447e99d5b23e3548c84c
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-06-23 10:58:40 +0530

    AnyKernel : Adapt for Pissaro

--- anykernel.sh
+++ anykernel.sh
@@ -10,27 +10,10 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=RMX2151
-device.name2=rmx2151
-device.name3=rmx2161
-device.name4=RMX2161
-device.name5=RMX2001
-device.name6=RM6785
-device.name7=rm6785
-device.name8=wasabi 
-device.name9=RMX2002
-device.name10=rmx2001
-device.name11=rmx2001L1
-device.name12=RMX2001L1
-device.name13=rmx2002
-device.name14=rmx2002L1
-device.name15=RMX2002L1
-device.name16=RMX2003
-device.name17=IN2023
-device.name18=RMX3031
-device.name19=GM1917
-supported.versions=
-supported.patchlevels=
+device.name1=pissarro
+device.name2=pissarroin
+device.name3=pissarroinpro
+supported.versions=11.0-12.0
 '; } # end properties
 
 # shell variables
