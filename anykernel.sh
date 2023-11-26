7f25f493ff1eb4e73fad2e7f6af1a64d2fe7bdff
Wahid Khan <wahidzk0091@gmail.com>
2023-11-26 17:15:52 +0000

    Anykernel: NoVA Bringup
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,8 +4,8 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Welcome To The AbzRaider Kernel ...
-do.devicecheck=0
+kernel.string=NoVA by @Abdul7852
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
@@ -13,7 +13,8 @@ do.cleanuponabort=0
 device.name1=pissarro
 device.name2=pissarroin
 device.name3=pissarroinpro
-
+supported.versions=
+supported.patchlevels=
 '; } # end properties
 
 # shell variables
@@ -28,56 +29,13 @@ patch_vbmeta_flag=auto;
 . tools/ak3-core.sh;
 
 
-ui_print " " "Patching system's build prop for fuse passthrough..."
-# FUSE Passthrough
-patch_prop /system/build.prop "persist.sys.fuse.passthrough.enable" "true"
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
+ui_print " » Flashing....";
+
 ## AnyKernel boot install
 dump_boot;
-
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
-write_boot;
-## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
-
